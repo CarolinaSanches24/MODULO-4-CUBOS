6bdc39d2456c36139694205d38d381778296440c
CarolinaSanches24 <carolmosilva34@gmail.com>
2023-11-07 19:46:19 -0300

    Teste

--- DEPLOY/src/index.js
+++ DEPLOY/src/index.js
@@ -4,6 +4,9 @@ require("dotenv").config({
 const express = require("express");
 
 const server = express();
+server.get("/", async (req, res) => {
+  return res.json({ mensagem: "Ok" });
+});
 const porta = process.env.PORT || 3000;
 server.listen(porta, () => {
   console.log(`Servidor Rodando na porta ${porta}`);
